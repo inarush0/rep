2074574aa4f5f303d726c901a016861a7ffd15eb
Andrew Rush <inarush0@gmail.com>
2016-12-18 22:44:16 -0500

    created update script used for deployment on DO droplet

---
+++ update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker pull $1/rep-prod:latest
+if docker stop rep-app; then docker rm rep-app; fi
+docker run -d -p 8080:8080 --name rep-app $1/rep-prod
+if docker rmi $(docker images --filter "dangling=true" -q --no-trunc); then :; fi
